6fab9af8cdd1df98d5e4ae6c33c709c26a1446a1
Daniel Haile <dannymom3@gmail.com>
2021-03-30 21:16:27 -0500

    updated

--- CS303_OOP/Labs/labW2D1-higherOrder/higherOrderTests.js
+++ CS303_OOP/Labs/labW2D1-higherOrder/higherOrderTests.js
@@ -13,20 +13,6 @@
 // const Calculator = myExports.Calculator;
 
 
-
-// describe("sort by age", function() {
-//     it("The sort of ages 25, 30, and 29 is [John, Mary, Pete]", function() {
-//         let john = { name: "John", age: 25 };
-//         let pete = { name: "Pete", age: 30 };
-//         let mary = { name: "Mary", age: 28 };
-//         let arr = [pete, john, mary];
-//         sortByAge(arr);
-//         assert.deepEqual(sortByAge(arr), john, mary, pete);
-//     });
-
-// });
-
-
 describe("Test myMap function passed array and function", function() {
     it("Array and function triple the given array", function() {
         let input = [2, 3, 4, 6];
@@ -38,17 +24,3 @@ describe("Test myMap function passed array and function", function() {
     });
 
 });
-
-// describe("filterRange", function() {
-//     it("returns the filtered values", function() {
-//         let arr = [5, 3, 8, 1];
-//         let filtered = filterRange(arr, 1, 4);
-//         assert.deepEqual(filtered, [3, 1]);
-//     });
-
-//     it("doesn't change the array", function() {
-//         let arr = [5, 3, 8, 1];
-//         let filtered = filterRange(arr, 1, 4);
-//         assert.deepEqual(arr, [5, 3, 8, 1]);
-//     });
-// });
