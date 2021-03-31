26c620da768c4f1788822114c4b36e259b9d7378
Daniel Haile <dannymom3@gmail.com>
2021-03-30 21:36:54 -0500

    updated

--- CS303_OOP/Labs/labW2D1-higherOrder/higherOrderTests.js
+++ CS303_OOP/Labs/labW2D1-higherOrder/higherOrderTests.js
@@ -2,6 +2,8 @@
 
 
 
+
+
 /* global assert    */
 /* comment out the node specific code when going to the browser*/
 // const assert = require("assert"); //always need this with node
@@ -21,6 +23,22 @@ describe("Test myMap function passed array and function", function() {
             return value * 3
         }
         assert.deepEqual(myMap(input, mapped), [6, 9, 12, 18]);
+    });
+
+});
+
+describe("Test myFilter function passed array and function", function() {
+    it("Array and function triple the given array", function() {
+        let arr = [2, 4, 3, 6, 9, 20];
+        assert.deepEqual(myFilter(arr, filtered), [2, 4, 6, 20]);
+    });
+
+});
+
+describe("Test myReduce function passed array and function", function() {
+    it("Array and function triple the given array", function() {
+        let inputVal = [3, 5, 20, 8, 6];
+        assert.deepEqual(myReduce(inputVal, reduced, 0), 42);
     });
 
 });
--- CS303_OOP/Labs/labW2D1-higherOrder/higherOrders.js
+++ CS303_OOP/Labs/labW2D1-higherOrder/higherOrders.js
@@ -1,7 +1,7 @@
 "use strict"
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { myMap, mapped };
+module.exports = { myMap, mapped, myFilter, filtered, myReduce, reduced };
 
 /*
 Write your own version of Array.map. Write a function, myMap(arr, fun) that takes
@@ -9,7 +9,7 @@ Write your own version of Array.map. Write a function, myMap(arr, fun) that take
  formed by applying the function to each element of the array and saving the 
  result in the new array. Do not use the Array.map method in your code.
 */
-
+//TODO - Write your own version of map.
 function myMap(arr, fun) {
 
     let newArr = [];
@@ -25,3 +25,43 @@ function mapped(value) {
 
 // let input = [2, 3, 4, 6];
 // console.log(myMap(input, mapped)) //[6, 9, 12, 18]
+//TODO - Write your own version of filter.
+function myFilter(arr, func) {
+    let filtArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let value = func(arr[i]);
+        if (value) {
+            filtArr.push(func(arr[i]))
+        }
+
+    }
+    return filtArr;
+}
+
+function filtered(value) {
+    if (value % 2 === 0) {
+        return value;
+    }
+}
+
+let arr = [2, 4, 3, 6, 9, 20];
+console.log(myFilter(arr, filtered)) //[2, 4, 6, 20]
+
+//TODO - Write your own version of reduce.
+
+function myReduce(arr, redFunc, init) {
+
+    let accum = init;
+    for (let i = 0; i < arr.length; i++) {
+        accum = redFunc(accum, arr[i])
+    }
+
+    return accum
+}
+
+function reduced(accum, value) {
+    return accum + value;
+}
+
+let inputVal = [3, 5, 20, 8, 6];
+console.log(myReduce(inputVal, reduced, 0)) // 42
