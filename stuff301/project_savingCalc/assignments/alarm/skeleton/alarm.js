86b645696ea0256e307f302d6e54af4f0ac8177b
Daniel Haile <dannymom3@gmail.com>
2021-04-18 16:34:46 -0500

    updated

--- stuff301/project_savingCalc/assignments/alarm/skeleton/alarm.js
+++
@@ -1,9 +0,0 @@
-function okayClick() {
-    // let btn = document.getElementById("go");
-    // btn.className = "awesome";
-    setTimeout("red", 3000);
-}
-
-window.onload = () => {
-    document.getElementById("go").onclick = okayClick;
-};
